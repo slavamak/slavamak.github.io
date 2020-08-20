60cc05daf8474244a34119ed78ef54e5d9ab684e
slavamak <slavamak.dev@gmail.com>
2020-08-21 00:32:11 +0300

    Initial build subtree commit

---
+++ build/precache-manifest.04a9509c11389cc4c4d529c5bc79529c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357697a6f179814cd26324aa8eab4f17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c833a038e38fa7f89648",
+    "url": "/static/css/main.531d1934.chunk.css"
+  },
+  {
+    "revision": "eaed3a05d48e813c3555",
+    "url": "/static/js/2.7fbbf0d9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.7fbbf0d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c833a038e38fa7f89648",
+    "url": "/static/js/main.549f5afd.chunk.js"
+  },
+  {
+    "revision": "7c1e69cd512176db0606",
+    "url": "/static/js/runtime-main.c8269756.js"
+  }
+]);
