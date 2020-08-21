c35c8101d86191aa3d7ef1ac66bfb4be498d1aee
slavamak <slavamak.dev@gmail.com>
2020-08-21 13:52:34 +0300

    added hooks

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,60 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Link from '../Link/Link';
 
 import './projects.css';
 
-class Projects extends Component {
+function Projects() {
+  const [list, setList] = useState(null);
 
-  state = {
-    list: null
-  };
+  useEffect(() => {
+    fetch('/projects.json')
+      .then(response => response.json())
+      .then(json => setList(json.list));
+  }, []);
 
-  async componentDidMount() {
-    let response = await fetch('/projects.json');
-    let projects = await response.json();
-
-    this.setState({
-      list: projects.list
-    });
-  };
-
-  render() {
-    const { list } = this.state;
-
-    if (list === null) {
-      return (
-        <section className="projects" id="projects">
-          <div className="container">
-            <h2 className="h2 projects__heading">Latest projects / Loading..</h2>
-          </div>
-        </section>
-      )
-    };
-
+  if (list === null) {
     return (
       <section className="projects" id="projects">
         <div className="container">
           <h2 className="h2 projects__heading">Latest projects</h2>
-          <ul className="projects-list">
-            {
-              list.map(({ title, url, description, technologies }, index) => (
-                <li className="projects-list__item" key={ index }>
-                  <div className="projects-list__item-details">
-                    <Link className="projects-list__item-title" url={ url } title={ title } target="_blank">
-                      <h3 className="h3">{ title }</h3>
-                    </Link>
-                    <h4 className="h4 projects-list__item-description">{ description }</h4>
-                    <p className="projects-list__item-technologies">{ technologies }</p>
-                  </div>
-                </li>
-              )).reverse()
-            }
-          </ul>
+          <span>Loading...</span>
         </div>
       </section>
-    );
-  }
+    )
+  };
+
+  return (
+    <section className="projects" id="projects">
+      <div className="container">
+        <h2 className="h2 projects__heading">Latest projects</h2>
+        <ul className="projects-list">
+          {
+            list.map(({ title, url, description, technologies }, index) => (
+              <li className="projects-list__item" key={ index }>
+                <div className="projects-list__item-details">
+                  <Link className="projects-list__item-title" url={ url } title={ title } target="_blank">
+                    <h3 className="h3">{ title }</h3>
+                  </Link>
+                  <h4 className="h4 projects-list__item-description">{ description }</h4>
+                  <p className="projects-list__item-technologies">{ technologies }</p>
+                </div>
+              </li>
+            )).reverse()
+          }
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
