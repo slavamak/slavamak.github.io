8c6d43116d3926850d940bf118ac0ce262c2119c
slavamak <slavamak.dev@gmail.com>
2020-08-24 11:26:08 +0300

    Added subtree

---
+++ build/precache-manifest.af5de602edbee949cc6a5b9eaec2cf95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b3319c5f45ddc4393b7383bec05cb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1979bc04ef10de266fcb",
+    "url": "/static/css/main.531d1934.chunk.css"
+  },
+  {
+    "revision": "e62b53c583361fde12c5",
+    "url": "/static/js/2.cc765902.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cc765902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1979bc04ef10de266fcb",
+    "url": "/static/js/main.7820bde8.chunk.js"
+  },
+  {
+    "revision": "7c1e69cd512176db0606",
+    "url": "/static/js/runtime-main.c8269756.js"
+  }
+]);
