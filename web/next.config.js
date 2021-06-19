4b03aa5db69895932e5ef234202dfb39ef8edba2
Slava Maksimov <slavamak.com@gmail.com>
2021-06-19 19:57:37 +0300

    Disable the built-in linting step completely

--- web/next.config.js
+++ web/next.config.js
@@ -3,6 +3,11 @@ module.exports = {
     SANITY_PROJECT_DATASET: process.env.SANITY_PROJECT_DATASET,
     SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
   },
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["cdn.sanity.io"],
   },
