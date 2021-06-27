0b09322b0206c004d12448c86c4d37bb056efdef
Slava Maksimov <slavamak.com@gmail.com>
2021-06-27 18:17:59 +0300

    Add google analitics

--- web/src/pages/_app.jsx
+++ web/src/pages/_app.jsx
@@ -1,9 +1,30 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import "../styles/index.css"
 
+import { useRouter } from "next/router"
 import { ThemeProvider } from "next-themes"
+import { useEffect } from "react"
+
+import * as ga from "../libs/ga"
 
 function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    // When the component is mounted, subscribe to router changes
+    // and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
       <Component {...pageProps} />
